1633a4b099f82c3ae5148f8458b25a1847d5f3d1
Martin Mwangi <mwangimartin1904@gmail.com>
2022-07-08 22:17:32 +0300

    signout user

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,7 +1,9 @@
 // Signup
 import { app, firebaseStore, firebaseAuth } from "./init.js";
 
-console.log(firebaseStore, firebaseAuth);
+const auth = firebaseAuth.getAuth();
+
+console.log(firebaseAuth);
 
 const signupForm = document.querySelector("#signup-form");
 signupForm.addEventListener("submit", (e) => {
@@ -9,16 +11,25 @@ signupForm.addEventListener("submit", (e) => {
 
     // get user info
     const email = signupForm["signup-email"].value;
-	const password = signupForm["signup-password"].value;
-	
-	const auth = firebaseAuth.getAuth()
+    const password = signupForm["signup-password"].value;
 
     // sign up the user
-    firebaseAuth.createUserWithEmailAndPassword(auth, email, password).then((cred) => {
-        console.log(cred.user);
-        // close the signup modal & reset form
-        const modal = document.querySelector("#modal-signup");
-        M.Modal.getInstance(modal).close();
-        signupForm.reset();
+    firebaseAuth
+        .createUserWithEmailAndPassword(auth, email, password)
+        .then((cred) => {
+            console.log(cred.user);
+            // close the signup modal & reset form
+            const modal = document.querySelector("#modal-signup");
+            M.Modal.getInstance(modal).close();
+            signupForm.reset();
+        });
+});
+
+// logout
+const logout = document.querySelector("#logout");
+logout.addEventListener("click", (e) => {
+    e.preventDefault();
+    firebaseAuth.signOut(auth).then(() => {
+        console.log("user signed out");
     });
 });
--- scripts/init.js
+++ scripts/init.js
@@ -19,4 +19,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-export { app, firebaseStore, firebaseAuth };
+export { app, firebaseStore, firebaseAuth, analytics };
